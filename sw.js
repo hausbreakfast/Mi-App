da3ac17a151d14ce13d3b5c5142fbabd9c479fc4
hausbreakfast <hausbreakfast@gmail.com>
2026-08-28 22:00:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,32 @@
+const CACHE_NAME = 'haus-app-v1';
+const ASSETS = [
+  '/Mi-App/',
+  '/Mi-App/index.html',
+  '/Mi-App/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Instalado');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
